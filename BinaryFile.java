c448ea2831aa4301ebac82b0d24021e11cb83de3
Irthn1311 <nhuutri1311@gmail.com>
2024-11-12 15:49:02 +0700

    BinaryFile cua co

---
+++ BinaryFile.java
@@ -0,0 +1,50 @@
+import java.io.DataOutputStream;
+import java.io.DataInputStream;
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class BinaryFile {
+    void ghiFile() throws IOException {
+        DataOutputStream outStream = new DataOutputStream(new FileOutputStream("teo.txt", true));
+        for (int i = 0; i < 10; i++) {
+            outStream.writeUTF("Hello");
+            outStream.writeInt((int) (20 + Math.random() * 25));
+            outStream.writeDouble(Math.random() * 500);
+        }
+        outStream.close();
+    }
+    
+    void docFile() {
+        FileInputStream f;
+        try {
+            f = new FileInputStream("teo.txt");
+            DataInputStream inStream = new DataInputStream(f);
+            try {
+                while (true) {
+                    String name = inStream.readUTF();
+                    int age = inStream.readInt();
+                    double salary = inStream.readDouble();
+                    System.out.println("Name: " + name + ", Age: " + age + ", Salary: " + salary);
+                }
+            } catch (EOFException e) {
+                // End of file reached
+            } finally {
+                inStream.close();
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e);
+        }
+    }
+
+    public static void main(String[] args) {
+        BinaryFile binaryFile = new BinaryFile();
+        try {
+            binaryFile.ghiFile();
+            binaryFile.docFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
