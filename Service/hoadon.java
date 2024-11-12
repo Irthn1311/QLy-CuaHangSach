9c25bbe91a8336c2123f0af3d89101af02d70853
Irthn1311 <nhuutri1311@gmail.com>
2024-11-12 16:41:15 +0700

    hoanthien

--- Service/hoadon.java
+++ Service/hoadon.java
@@ -5,4 +5,48 @@ public class hoadon {
     private String makhachhang;
     private String manhanvien;
     private int tongtien;
+    public hoadon(){
+        mahoadon="";
+        ngaytaohoadon="";
+        makhachhang="";
+        manhanvien="";
+        tongtien=0;
+    }
+    public hoadon(String mahoadon,String ngaytaohoadon,String makhachhang,String manhanvien,int tongtien){
+        this.mahoadon=mahoadon;
+        this.ngaytaohoadon=ngaytaohoadon;
+        this.makhachhang=makhachhang;
+        this.manhanvien=manhanvien;
+        this.tongtien=tongtien;
+    }
+    public String getMahoadon(){
+        return mahoadon;
+    }
+    public String getNgaytaohoadon(){
+        return ngaytaohoadon;
+    }
+    public String getMakhachhang(){
+        return makhachhang;
+    }
+    public String getManhanvien(){
+        return manhanvien;
+    }
+    public int getTongtien(){
+        return tongtien;
+    }
+    public void setMahoadon(String mahoadon){
+        this.mahoadon=mahoadon;
+    }
+    public void setNgaytaohoadon(String ngaytoahoadon){
+        this.ngaytaohoadon=ngaytoahoadon;
+    }
+    public void setMakhachhang(String makhachhang){
+        this.makhachhang=makhachhang;
+    }
+    public void setManhanvien(String manhanvien){
+        this.manhanvien=manhanvien;
+    }
+    public void setTongtien(int tongtien){
+        this.tongtien=tongtien;
+    }
 }
