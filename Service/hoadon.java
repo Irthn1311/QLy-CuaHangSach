4a28c188681eff4e56b9505f4b2abf343790206a
MinhQuan4k <minhquanygo@gmail.com>
2024-11-12 15:58:21 +0700

    hoasdon

---
+++ Service/hoadon.java
@@ -0,0 +1,8 @@
+package Service;
+public class hoadon {
+    private String mahoadon;
+    private String ngaytaohoadon;
+    private String makhachhang;
+    private String manhanvien;
+    private int tongtien;
+}
